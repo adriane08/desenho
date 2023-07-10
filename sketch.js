25c029ecfe169366bb4877e6e8d4f8e7eec24888
adriane08 <139055906+adriane08@users.noreply.github.com>
2023-07-10 11:14:13 -0300

    definindo css e js

---
+++ sketch.js
@@ -0,0 +1,30 @@
+function setup() {
+    createCanvas(600, 600);
+  }
+  
+  function draw() {
+    background(220);
+  }
+  function setup() {
+  
+    
+  createCanvas(600, 600);
+  
+  background("black");
+  
+  }
+  
+  function draw() {
+  
+  stroke("yellow");
+  
+  fill("blue")
+    
+  if(mouseIsPressed){
+  
+  rect(mouseX, mouseY, 20, 35);
+  
+  }
+  
+  }
+  
